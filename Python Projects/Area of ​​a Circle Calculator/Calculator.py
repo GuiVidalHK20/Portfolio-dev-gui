5a98062fcaa9ff36ac5e74e01641d2603904e6b7
Guilherme Vidal <guigashk2010@gmail.com>
2025-10-27 22:33:23 -0300

    Add circle area calculator in Portuguese and English Versions
    Added a circle area calculator that takes radius input and calculates the area in both Portuguese and English.

---
+++ Python Projects/Area of ​​a Circle Calculator/Calculator.py
@@ -0,0 +1,17 @@
+raio = int(input("Qual o valor do raio do círculo?"))
+
+pi = 3.14
+
+area = pi * (raio ** 2)
+
+print("A área é igual a", area)
+
+//
+
+radius = int((input("What is the value of the radius?")
+
+pi = 3.14
+
+area = pi * (radius ** 2)
+
+print("The area is equal to", area)
